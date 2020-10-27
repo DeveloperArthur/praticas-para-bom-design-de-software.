748cd36e76736446f16afd4aab9e265c6390a024
Arthur Santos Almeida <42553658+DeveloperArthur@users.noreply.github.com>
2020-10-27 16:14:15 -0300

    Delete Desconto.java

--- src/main/java/br/com/arthur/principles/chainofresponsibility/solucao/Desconto.java
+++
@@ -1,7 +0,0 @@
-package br.com.arthur.principles.designpatterns.chainofresponsibility.solucao;
-
-public interface Desconto {
-    double desconta(Orcamento orcamento);
-
-    void setProximo(Desconto proximo);
-}
