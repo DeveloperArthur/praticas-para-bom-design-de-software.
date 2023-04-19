df2d387d4f32d9e6759e9f2aabcd5080572ed97f
Arthur Santos Almeida <42553658+DeveloperArthur@users.noreply.github.com>
2023-04-19 11:52:12 -0300

    add pattern

--- src/main/java/br/com/arthur/principles/designpatterns/adapter/solucao/Main.java
+++ src/main/java/br/com/arthur/principles/designpatterns/adapter/solucao/Main.java
@@ -1,6 +1,9 @@
 /*
 Pronto, construimos uma interface e um adaptador, agora nosso main depende da interface
 e sim, o adapter vai depender da implementacao neste caso...
+
+outro exemplo de impl adapter em outro cenario: 
+https://github.com/DeveloperArthur/CQRS-with-RabbitMQ-Redis-InfraAsCode/blob/main/src/main/java/br/com/arthur/cqrs/infra/JsonUtilAdapterWithGson.java
 */
 
 package br.com.arthur.principles.designpatterns.adapter.solucao;
