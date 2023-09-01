58575e60e3e01c4960377605d33c31c43046bc28
Arthur Santos Almeida <42553658+DeveloperArthur@users.noreply.github.com>
2023-09-01 10:17:47 -0300

    ajustando

--- src/main/java/br/com/arthur/principles/padroes_arquiteturais_livro_martin_fowler/transaction_script/transactionscript.ts
+++ src/main/java/br/com/arthur/principles/padroes_arquiteturais_livro_martin_fowler/transaction_script/transactionscript.ts
@@ -1,26 +1,3 @@
-export default class ParkingService {
-    parkedCars: any = {};
-
-    constructor() {
-
-    }
-
-    async checkin (plate: string, checkinDate: Date) {
-        if (!plate.match(/[A-Z]{3}[0-9]{4}/)) throw new Error("Invalid plate");
-        this.parkedCars[plate] = { plate, checkinDate };
-    }
-
-    async checkout (plate: string, checkoutDate: Date) {
-        const parkedCar = this.parkedCars[plate];
-        if (!parkedCar) throw new Error("Parked car not found");
-        const duration = (checkoutDate.getTime() - parkedCar.checkinDate.getTime()) / (1000 * 60 * 60);
-        const price = duration * 10;
-        return {
-            price
-        }
-    }
-}
-
 /*
 O padrão Transaction Script é um padrão de projeto de software que lida com a lógica de negócios 
 de uma aplicação por meio de scripts que representam transações individuais. 
@@ -50,6 +27,7 @@ relativamente direta. Mudanças na lógica de negócios de uma transação espec
 afetar outras partes do sistema.
 
 
+
 Então pensando em uma API Rest, usando o padrão Transaction Script, a lógica de negócios é implementada de
 forma procedural, muitas vezes diretamente no controlador ou na camada que lida com as rotas da API. 
 Não há uma abstração significativa das regras de negócios em objetos ou camadas separadas, como é feito em 
@@ -68,6 +46,85 @@ A falta de abstração pode levar a dificuldades de manutençãocódigo duplicad
 Portanto, em sistemas mais complexos, é mais comum recorrer a abordagens que separam a lógica de negócios 
 em camadas mais definidas, como o padrão Service Layer, onde as regras de negócios são encapsuladas em 
 serviços independentes, ou o padrão Domain Model, onde os objetos de domínio representam conceitos do mundo real.
+
+
+
+O código abaixo é um exemplo de Transaction Script 
+*/
+
+export default class ParkingService {
+    parkedCars: any = {};
+
+    constructor() {
+
+    }
+
+    async checkin (plate: string, checkinDate: Date) {
+        if (!plate.match(/[A-Z]{3}[0-9]{4}/)) throw new Error("Invalid plate");
+        this.parkedCars[plate] = { plate, checkinDate };
+    }
+
+    async checkout (plate: string, checkoutDate: Date) {
+        const parkedCar = this.parkedCars[plate];
+        if (!parkedCar) throw new Error("Parked car not found");
+        const duration = (checkoutDate.getTime() - parkedCar.checkinDate.getTime()) / (1000 * 60 * 60);
+        const price = duration * 10;
+        return {
+            price
+        }
+    }
+}
+
+/*
+Pois há duas funções principais: checkin e checkout, que representam transações individuais 
+dentro de um sistema de estacionamento. Aqui estão as características que tornam esse 
+código um exemplo de Transaction Script:
+
+- Lógica de Negócios em Scripts Separados: As operações de checkin e checkout são tratadas como scripts separados
+onde cada um lida com uma única transação (entrada ou saída de um carro do estacionamento).
+
+- Procedural e Direto: A lógica dentro desses scripts é mais direta e procedural. O foco está nas operações 
+sequenciais que precisam ser realizadas para concluir cada transação.
+
+- Acesso a Dados Simplificado: Os dados são armazenados em um objeto chamado parkedCars, que é um repositório 
+simples para manter as informações dos carros estacionados.
+
+- Ausência de Abstração Complexa: Não há camadas distintas de serviço ou classes de domínio altamente abstraídas. 
+A lógica de negócios é implementada dentro desses scripts de transação.
+
+- Regras de Negócios Diretas: A lógica de cálculo de preço com base na duração de estacionamento é diretamente 
+incorporada no script checkout.
+
+
+
+Qualquer bloco de código procedural que não abstrai dados em objetos, não segue princípios de design orientado
+a objetos e não organiza a funcionalidade em classes ou módulos pode ser considerado Transaction Script 
+porém depende, por exemplo, o código abaixo NÃO é um Transaction Script:
+*/
+
+function calcularSalarioLiquido(salarioBruto: number, taxaImpostos: number): number {
+    const descontoImpostos: number = salarioBruto * taxaImpostos;
+    const salarioLiquido: number = salarioBruto - descontoImpostos;
+    return salarioLiquido;
+}
+
+/*
+É uma função que realiza um cálculo simples do salário líquido com base no salário bruto e na taxa de impostos. 
+Embora seja procedural e direto, ele não se encaixa na definição de um Transaction Script porque não está tratando 
+de uma transação ou operação de negócios específica. Em vez disso, ele está executando uma tarefa matemática simples.
+
+O Transaction Script, como mencionado anteriormente, é uma abordagem que lida com transações individuais ou operações
+de negócios específicas. Sua função é mais apropriada para operações mais complexas, como processamento de pedidos
+manipulação de transações financeiras, etc., onde há um conjunto de ações coordenadas a serem executadas.
+
+Portanto, ter um bloco de código procedural ou uma parte do código que usa um estilo mais procedural não torna 
+automaticamente essa parte um Transaction Script, a menos que ela seja projetada para lidar com transações individuais 
+simples de forma direta e sequencial. 
+
+Mas supondo que eu tenha meu projeto inteiro onde eu uso abstração de dados em objetos, sigo princípios 
+de design orientado a objetos e organizo a funcionalidade em classes, eu posso ter um Transaction Script em 
+algum lugar especifico do meu código sim.
+
 
 
 Transaction Script pode ser considerado um "code smell" (sinal de código problemático) em muitos cenários
@@ -94,38 +151,11 @@ ela está fortemente acoplada ao restante do código.
 pode dificultar a evolução da aplicação de forma coerente.
 
 
+
 Geralmente dizem que devemos escolher entre TransactionScript e Domain Model, isso porque no Domain Model 
 nossas classes de domínio espelham tabelas do banco de dados, e são modelos ricos, tem validações, tem regras 
 de negócio encapsulados, e o Transaction Script não utiliza objetos, é procedural.
 
-
-Qualquer bloco de código procedural que não abstrai dados em objetos, não segue princípios de design orientado
-a objetos e não organiza a funcionalidade em classes ou módulos é considerado Transaction Script porém
-depende, por exemplo, o código abaixo NÃO é um Transaction Script:
-*/
-
-function calcularSalarioLiquido(salarioBruto: number, taxaImpostos: number): number {
-    const descontoImpostos: number = salarioBruto * taxaImpostos;
-    const salarioLiquido: number = salarioBruto - descontoImpostos;
-    return salarioLiquido;
-}
-
-/**
-É uma função que realiza um cálculo simples do salário líquido com base no salário bruto e na taxa de impostos. 
-Embora seja procedural e direto, ele não se encaixa na definição de um Transaction Script porque não está tratando 
-de uma transação ou operação de negócios específica. Em vez disso, ele está executando uma tarefa matemática simples.
-
-O Transaction Script, como mencionado anteriormente, é uma abordagem que lida com transações individuais ou operações
-de negócios específicas. Sua função é mais apropriada para operações mais complexas, como processamento de pedidos
-manipulação de transações financeiras, etc., onde há um conjunto de ações coordenadas a serem executadas.
-
-Portanto, ter um bloco de código procedural ou uma parte do código que usa um estilo mais procedural não torna 
-automaticamente essa parte um Transaction Script, a menos que ela seja projetada para lidar com transações individuais 
-simples de forma direta e sequencial. 
-
-Mas supondo que eu tenha meu projeto inteiro onde eu uso abstração de dados em objetos, sigo princípios 
-de design orientado a objetos e organizo a funcionalidade em classes, eu posso ter um Transaction Script em 
-algum lugar especifico do meu código sim.
 
 
 Em resumo, o padrão Transaction Script é uma abordagem específica, simples e direta para lidar com a 
